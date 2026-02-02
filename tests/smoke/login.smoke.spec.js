5230050cae42f99b61a40cc8e330829eaff2a3ec
Vidhi Chaudhary <chaudhary.vidhi07@gmail.com>
2026-02-03 02:41:02 +0530

    updates login tests to utilize pre-test setup

--- tests/smoke/login.smoke.spec.js
+++ tests/smoke/login.smoke.spec.js
@@ -3,16 +3,8 @@ const { LoginPage } = require('../../pages/LoginPage');
 const { ProductsPage } = require('../../pages/ProductsPage');
 
 test.describe('@smoke @login', () => {
-  test('User can login successfully', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const productsPage = new ProductsPage(page);
-
-    await loginPage.navigate();
-    await loginPage.login('standard_user', 'secret_sauce');
-
-    // validate Product Page is loaded correctly
-    const isProductsPageLoaded = await productsPage.isLoaded();
-    await expect(isProductsPageLoaded).toBeTruthy();
+  test('User is already logged in using storage state', async ({ page }) => {
+    await page.goto('/inventory.html');
     await expect(page).toHaveURL(/inventory.html/);
     await expect(page.locator('.app_logo')).toBeVisible();
   });
